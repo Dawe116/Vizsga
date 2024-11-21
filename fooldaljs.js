3a90f9eb9f0f681a8d4a0faadccb0ac5839f5d8b
Dawe116 <molnar.dawe116@gmail.com>
2024-11-21 19:12:53 +0100

    kezdet

---
+++ fooldaljs.js
@@ -0,0 +1,6 @@
+Search= ()=>{
+document.getElementById("search").addEventListener("onmouseenter").style.backgroundColor = "red";
+document.getElementById("search").addEventListener("onmouseleave").style.backgroundColor = "white";
+};
+
+
