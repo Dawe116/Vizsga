15ecdd260c085cd5cdd04547a6f895412d55f02a
Dawe116 <molnar.dawe116@gmail.com>
2024-12-02 09:51:54 +0100

    fo oldal kezdete

--- fooldaljs.js
+++ fooldaljs.js
@@ -1,6 +1,11 @@
-Search= ()=>{
-document.getElementById("search").addEventListener("onmouseenter").style.backgroundColor = "red";
-document.getElementById("search").addEventListener("onmouseleave").style.backgroundColor = "white";
-};
+const sidebar = document.getElementById('sidebar');
+const toggleButton = document.getElementById('toggleButton');
+const content = document.getElementById('content');
+
+toggleButton.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+    toggleButton.classList.toggle('collapsed');
+    toggleButton.textContent = sidebar.classList.contains('collapsed') ? 'Show Filters' : 'Hide Filters';
+});
 
 
