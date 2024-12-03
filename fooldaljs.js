ffcc4b69d2d90129f68fa1bc044a804969d0ae63
Dawe116 <144417472+Dawe116@users.noreply.github.com>
2024-12-03 08:38:23 +0100

    .

--- fooldaljs.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.getElementById('sidebar');
-const toggleButton = document.getElementById('toggleButton');
-const content = document.getElementById('content');
-
-toggleButton.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-    toggleButton.classList.toggle('collapsed');
-});
