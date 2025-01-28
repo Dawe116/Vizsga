d18d62e7b3148af9f1ef925cf6f309461716122e
Dawe116 <molnar.dawe116@gmail.com>
2025-01-28 12:09:10 +0100

    Asztali elkezdése, Login window egy része

---
+++ FoodifyWPF/FoodifyWPF/DTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using FoodifyWPF.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FoodifyWPF.DTOs
+{
+    public class LoginDTO
+    {
+        public string LoginName { get; set; }
+
+        public string TmpHash { get; set; }
+
+    }
+}
---
+++ FoodifyWPF/FoodifyWPF/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using FoodifyWPF.Models;
+using FoodifyWPF.Windows;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FoodifyWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string uId = "";
+        static int SaltLength = 64;
+
+        #region Salt, SHA256
+
+        public static Dictionary<string, User> LoggedInUsers = new Dictionary<string, User>();
+
+        public static string GenerateSalt()
+        {
+            Random random = new Random();
+            string karakterek = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string salt = "";
+            for (int i = 0; i < SaltLength; i++)
+            {
+                salt += karakterek[random.Next(karakterek.Length)];
+            }
+            return salt;
+        }
+
+        public static string CreateSHA256(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] data = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+                return sBuilder.ToString();
+            }
+        }
+        #endregion
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenLoginWindow_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.ShowDialog();
+        }
+    }
+}
