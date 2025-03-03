f8344b338b29e05fccd265a6ded9f82ad36f8dea
Dawe116 <molnar.dawe116@gmail.com>
2025-03-03 13:02:48 +0100

    Kateróriára való osztás

--- foodifyreact/src/Kategoriak/GyrosLista.jsx
+++ foodifyreact/src/Kategoriak/GyrosLista.jsx
@@ -8,7 +8,7 @@ export const GyrosLista = () => {
     const [data, setData] = useState([]); 
   
     useEffect(() => {
-      axios.get("http://localhost:5000/api/Restaurant")
+      axios.get("http://localhost:5000/api/Restaurant/category/Gyros")
         .then(response => {
           setData(response.data);
           console.log(response.data);
