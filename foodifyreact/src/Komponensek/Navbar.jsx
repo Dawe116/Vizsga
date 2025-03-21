b935acc481d66848b5abd618c2ea7ab162b11759
TAdrianD <tadriand12@gmail.com>
2025-03-21 12:09:47 +0100

    Navbar v5

--- foodifyreact/src/Komponensek/Navbar.jsx
+++ foodifyreact/src/Komponensek/Navbar.jsx
@@ -40,7 +40,6 @@ const Navbar = ({ token, setToken, logged, setLogged, onSearch }) => {
                 <>
                   <li><Link to="/Fiok" onClick={() => setMenuOpen(false)}>Saját fiók</Link></li>
                   <li><Link to="/Kosar" onClick={() => setMenuOpen(false)}>Kosaram</Link></li>
-                  <li><Link to="/Elfelejtett" onClick={() => setMenuOpen(false)}>Elfelejtett jelszó</Link></li>
                   <li onClick={() => {
                     localStorage.removeItem("token");
                     setToken("");
