ab08fbc7f6450bebc1ed7b07a14cbb0995ce28ea
TAdrianD <tadriand12@gmail.com>
2025-04-03 20:51:11 +0200

    Rendelés javítás

--- foodifyreact/src/Oldalak/Rendeles.jsx
+++ foodifyreact/src/Oldalak/Rendeles.jsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import { KosarTartalom } from "../Komponensek/KosarTartalom";
 import '../Stilusok/Rendeles.css';
 import Footer from '../Komponensek/Footer';
-import emailjs from "emailjs-com";
 
 const Rendeles = () => {
     const { restaurantId } = useParams();
@@ -14,6 +13,7 @@ const Rendeles = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [modalContent, setModalContent] = useState({});
     const navigate = useNavigate();
+
     const { kosar, addToCart, removeFromCart, clearCart } = useContext(KosarTartalom);
 
     useEffect(() => {
@@ -47,10 +47,7 @@ const Rendeles = () => {
     const placeOrder = () => {
         const token = localStorage.getItem("token");
         const addresses = JSON.parse(localStorage.getItem("cimek") || "[]");
-        
-        const user = JSON.parse(localStorage.getItem("user") || "{}");
-        const userId = user.id;
-    
+
         if (!token) {
             setModalContent({
                 message: ["A rendelés leadása előtt be kell jelentkeznie."],
@@ -61,78 +58,30 @@ const Rendeles = () => {
             return;
         }
 
-    
-        const totalPrice = kosar.reduce((sum, item) => sum + item.price * item.quantity, 0);
-        const orderDetails = kosar.map(item => `${item.name} x${item.quantity} - ${item.price * item.quantity} Ft`).join("\n");
-        const storedAddresses = JSON.parse(localStorage.getItem("cimek") || "[]");
-
-        if (!storedAddresses || storedAddresses.length === 0 || 
-            !storedAddresses[0].street || !storedAddresses[0].city || !storedAddresses[0].postalCode) {
+        if (!addresses || addresses.length === 0) {
             setModalContent({
-                message: ["Kérjük, ellenőrizze a kiszállítási címét. Ha nincs cím, adja meg a rendelés leadása előtt."],
+                message: ["Kérjük, ellenőrizze a kiszállítási címét. Hogyha hiányos akkor adja meg a kiszállítási címét a rendelés leadásához."],
                 buttonText: "Saját fiók",
                 buttonAction: () => navigate("/fiok")
             });
             setIsModalOpen(true);
             return;
         }
-        
-        const deliveryAddress = `${storedAddresses[0].street}, ${storedAddresses[0].city}, ${storedAddresses[0].postalCode}`;
-        
-    
-        if (userId) {
-            axios.get(`https://localhost:5000/api/Users/${userId}`)
-                .then(response => {
-                    const user = response.data;
-                    const emailParams = {
-                        to_email: user.email,
-                        order_details: orderDetails,
-                        total_price: `${totalPrice} Ft`,
-                        delivery_address: deliveryAddress
-                    };
-    
-                    emailjs.send("service_kx0t42a", "template_b5as04l", emailParams, "BAFfkL_eMj75pPXcR")
-                        .then(() => {
-                            setModalContent({
-                                message: [
-                                    "A rendelést átadtuk a kiszállító partnerünknek.",
-                                    `Teljes fizetendő összeg: ${totalPrice} Ft.`,
-                                    "A rendelés visszaigazolását elküldtük az e-mail címére.",
-                                    "Köszönjük, hogy a Foodify-al rendelt!"
-                                ],
-                                buttonText: "Rendben",
-                                buttonAction: () => { setIsModalOpen(false); clearCart(); navigate("/FoodifyHome"); }
-                            });
-                            setIsModalOpen(true);
-                        })
-                        .catch(() => {
-                            setModalContent({
-                                message: ["Hiba történt az e-mail küldése során. Kérjük, próbálja újra később."],
-                                buttonText: "Bezárás",
-                                buttonAction: () => setIsModalOpen(false)
-                            });
-                            setIsModalOpen(true);
-                        });
-                })
-                .catch(error => {
-                    console.error("Hiba történt a felhasználói adatok lekérésekor:", error);
-                    setModalContent({
-                        message: ["Hiba történt a felhasználói adatok lekérésekor. Kérjük, próbálja újra később."],
-                        buttonText: "Bezárás",
-                        buttonAction: () => setIsModalOpen(false)
-                    });
-                    setIsModalOpen(true);
-                });
-        } else {
-            setModalContent({
-                message: ["Nem található felhasználói adat. Kérjük, jelentkezzen be."],
-                buttonText: "Bejelentkezés",
-                buttonAction: () => navigate("/bejelentkezes")
-            });
-            setIsModalOpen(true);
-        }
+
+        const totalPrice = kosar.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+        setModalContent({
+            message: [
+                "A rendelést átadtuk a kiszállító partnerünknek.",
+                `Teljes fizetendő összeg: ${totalPrice} Ft.`,
+                "Köszönjük, hogy a Foodify-al rendelt!"
+            ],
+            buttonText: "Rendben",
+            buttonAction: () => { setIsModalOpen(false); clearCart(); navigate("/FoodifyHome"); }
+        });
+        setIsModalOpen(true);
     };
-                
+
     return (
         <div id="root">
             <div className="order-container">
@@ -168,7 +117,7 @@ const MenuItemCard = ({ menu, addToCart }) => {
     };
 
     return (
-        <div className="menu-card" style={{ backgroundImage: `url(data:image/png;base64,${menu.picture})` }}>
+        <div className="menu-card" style={{backgroundImage: `url(data:image/png;base64,${menu.picture})` }}>
             <div className="menu-content">
                 <h3 className="menu-name">{menu.name}</h3>
                 <div className="menu-description">
